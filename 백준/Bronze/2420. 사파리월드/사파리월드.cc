503b7b9406d68993c34330a8845c61dd5d0d12a4
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2024-10-20 14:06:20 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long int n, m;
+	cin >> n >> m;
+
+	cout << abs(n - m);
+
+}
