aae4ea3f80d709f41490e0032fc824476bb2128e
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2024-10-23 16:50:12 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,19 +1,8 @@
 #include <iostream>
-#include <string>
+#include <vector>
 #include <climits>
 
 using namespace std;
-
-string BW[8] = {
-	"BWBWBWBW",
-	"WBWBWBWB",
-	"BWBWBWBW",
-	"WBWBWBWB",
-	"BWBWBWBW",
-	"WBWBWBWB",
-	"BWBWBWBW",
-	"WBWBWBWB"
-};
 
 string WB[8] = {
 	"WBWBWBWB",
@@ -26,30 +15,57 @@ string WB[8] = {
 	"BWBWBWBW"
 };
 
-string Board[50];
+string BW[8] = {
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB",
+	"BWBWBWBW",
+	"WBWBWBWB"
+};
 
-int BWCount(int a, int b)
+//밑에서 char CharBoard[50][50];로 선언하고 2차원 문자배열을
+// 함수 매개변수로 보낸다고 할때
+//1. 만약 값으로 CharList를 보내고 싶다면
+//    int CountWB(char CharList[50][50], int y, int x);
+//2.  만약 참조로 CharList를 보내고 싶다면
+//int CountWB(const char(&CharList)[50][50], int y, int x);
+// ** 주의 char (&CharList)[][] 와 같이 배열 크기를 명시 하지 않으면 컴파일 에러가 남.
+// C++ 컴파일러가 배열의 크기를 명시적으로 알아야 메모리 레이아웃을 정확하게 처리할 수 있기 때문
+// 대안으로 동적 배열 또는 템플릿을 사용
+// int CountWB(char** CharList, int y, int x, int rowSize, int colSize)
+// int CountWB(vector<vector<char>>& CharList, int y, int x)
+
+int CountWB(const vector<string>& CharList, int y, int x)
 {
 	int Count = 0;
 	for (int i = 0; i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
+		for (int k = 0; k < 8; k++)
 		{
-			if (BW[i][j] != Board[a + i][b + j])
+			int TempY = y + i;
+			int TempX = x + k;
+
+			if (WB[i][k] != CharList[TempY][TempX])
 				Count++;
 		}
 	}
 	return Count;
 }
 
-int WBCount(int a, int b)
+int CountBW(const vector<string>& CharList, int y, int x)
 {
 	int Count = 0;
 	for (int i = 0; i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
+		for (int k = 0; k < 8; k++)
 		{
-			if (WB[i][j] != Board[a + i][b + j])
+			int TempY = y + i;
+			int TempX = x + k;
+
+			if (BW[i][k] != CharList[TempY][TempX])
 				Count++;
 		}
 	}
@@ -62,28 +78,28 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int N, M; 
+	int N, M;
 	cin >> N >> M;
+	
+	//char CharBoard[50][50];
+	vector<string> Board(N);
 
 	for (int i = 0; i < N; i++)
 	{
 		cin >> Board[i];
 	}
 
-	int MinValue = INT_MAX;
-	for (int i = 0; i + 7 < N; i++)
+	int MinCount = INT_MAX;
+	for (int i = 0; i <= N - 8; i++)
 	{
-		for (int j = 0; j + 7 < M; j++)
+		for (int k = 0; k <= M - 8; k++)
 		{
-			int temp = min(BWCount(i, j), WBCount(i, j));
-			if (temp < MinValue)
-			{
-				MinValue = temp;
-			}
+			MinCount = min(MinCount, CountWB(Board, i, k));
+			MinCount = min(MinCount, CountBW(Board, i, k));
 		}
 	}
 
-	cout << MinValue;
+	cout << MinCount;
 
 	return 0;
 }
