80fd3fa8a0a1bf30517e773b948fb02246f4d56a
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2024-10-23 15:12:04 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,82 +1,87 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
+// 밭
+int Bat[51][51] = { 0 };
+bool Visited[51][51] = { false };
 
-int t, m, n, k, cnt = 0;
-int arr[51][51] = { 0, };
-// 상하좌우 세팅
-int dirX[] = { -1,1,0,0 };
-int dirY[] = { 0,0,-1,1 };
+// 상하좌우
+int Dy[4] = { -1, 1, 0, 0 };
+int Dx[4] = { 0, 0, -1, 1 };
 
-void dfs(int y, int x)
+void BFS(int y, int x, int YSize, int XSize)
 {
-   // 0으로 바꾸기(방문처리)
-	arr[y][x] = 0;
-     
-    // 상하좌우 탐색
-	for (int i = 0; i < 4; i++)
-	{
-		int _y = dirY[i] + y;
-		int _x = dirX[i] + x;
-        
-        // 갈 수 있는 좌표라면
-		if (_y >= 0 && _y < n && _x >= 0 && _x < m)
-		{
-            // 계산한 좌표가 배추 밭이라면
-			if (arr[_y][_x] == 1)
-				dfs(_y, _x);
-		}
-	}
-	
+    queue<pair<int, int>> Queue;
+    Queue.push({ y, x });
+    Visited[y][x] = true;
+
+    while (!Queue.empty())  // while 조건 수정
+    {
+        pair<int, int> Front = Queue.front();
+        Queue.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int NewY = Front.first + Dy[i];
+            int NewX = Front.second + Dx[i];
+
+            // 범위 체크
+            if (NewY < 0 || NewX < 0 || NewY >= YSize || NewX >= XSize)
+                continue;
+
+            if (Visited[NewY][NewX] == false && Bat[NewY][NewX] == 1)  // Bat[NewY][NewX] == 1 조건 추가
+            {
+                Visited[NewY][NewX] = true;
+                Queue.push({ NewY, NewX });
+            }
+        }
+    }
 }
 
 int main()
 {
-   // 테스트 케이스 개수 입력
-	cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
+    int T;
+    cin >> T;
 
-	int x, y;
+    for (int i = 0; i < T; i++)
+    {
+        int M, N, K;
+        cin >> M >> N >> K;
 
-	for (int o = 0; o < t; o++)
-	{
-		cnt = 0;
-        // 가로, 세로, 배수를 심은 밭 개수 
-		cin >> m >> n >> k;
+        // 배열 초기화 (범위를 초과하지 않도록 안전한 초기화)
+        fill(&Bat[0][0], &Bat[50][51], 0);
+        fill(&Visited[0][0], &Visited[50][51], false);
 
-        // 배열 초기화
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				arr[i][j] = 0;
-			}
-		}
-        
+        for (int j = 0; j < K; j++)
+        {
+            int X, Y;
+            cin >> X >> Y;
+            Bat[Y][X] = 1;
+        }
 
-		for (int i = 0; i < k; i++)
-		{
-            // 좌표 입력
-			cin >> x >> y;
-            // 해당 좌표 배추 밭 처리
-			arr[y][x] = 1;
-		}
+        // 지렁이 수
+        int Count = 0;
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{  
-                // 해당 좌표가 배추 밭이라면
-				if (arr[i][j] == 1)
-				{
-                    // DFS를 통해 연결되어 있는 배추 밭을 0으로 바꿈
-					dfs(i, j);
-                    // 개수를 셈
-					cnt++;
-				}
-			}
-		}
-        // 개수 출력
-		cout << cnt << '\n';
-	}
+        for (int y = 0; y < N; y++)
+        {
+            for (int x = 0; x < M; x++)
+            {
+                if (Bat[y][x] == 1 && Visited[y][x] == false)
+                {
+                    BFS(y, x, N, M);
+                    Count++;
+                }
+            }
+        }
+
+        cout << Count << "\n";
+    }
+
+    return 0;
 }
