a532b508fd2b7e683d023cde97bc7756a1bc658e
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2024-10-23 22:48:23 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -5,55 +5,51 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int N;
-	cin >> N;
+    int N;
+    cin >> N;
 
-	vector<int> A(N, 0);
+    vector<int> A(N, 0);
 
-	for (int i = 0; i < N; i++)
-	{
-		cin >> A[i];
-	}
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
 
-	sort(A.begin(), A.end());
+    sort(A.begin(), A.end());
 
-	int count = 0;
+    int count = 0;
 
-	for (int k = 0; k < N; k++)
-	{
-		long find = A[k];
-		int i = 0;
-		int j = N - 1;
+    for (int k = 0; k < N; k++) {
+        int find = A[k];
+        int i = 0;
+        int j = N - 1;
 
-		while (i < j)
-		{
-			if (A[i] + A[j] == find)
-			{
-				if (i != k && j != k)
-				{
-					count++;
-					break;
-				}
-				else if (i == k)
-					i++;
-				else if (j == k)
-					j--;
-			}
-			else if (A[i] + A[j] < find)
-			{
-				i++;
-			}
-			else
-			{
-				j--;
-			}
-		}
-	}
+        while (i < j) {
+            if (A[i] + A[j] == find) {
+                if (i != k && j != k) {
+                    count++;
+                    break;  // 하나의 k에 대해 한 번만 카운트
+                }
+                else if (i == k) {
+                    i++;
+                }
+                else if (j == k) {
+                    j--;
+                }
+            }
+            else if (A[i] + A[j] < find) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+    }
 
-	cout << count << "\n";
+    cout << count << "\n";
 
+    return 0;
 }
