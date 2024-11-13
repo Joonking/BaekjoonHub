437aa4faa2190a00432ec9f93f861bef180d8d75
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2024-11-13 11:58:17 +0900

    [Silver II] Title: DNA 비밀번호, Time: 16 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
@@ -1,109 +1,84 @@
-//009_12891 DNA 비밀번호
-
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int checkArr[4];				//비밀번호 체크 배열
-int myArr[4];				//현재 상태 배열
-int checkSecret = 0;		//몇 개의 문자에 대한 개수를 충족했는지 판단하는 변수
-void Add(char c);			//문자 더하기 함수
-void Remove(char c);		//문자 빼기 함수
-
-int main() {
+int main()
+{
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int S, P;  //S : 문자열 크기, P : 부분 문자열 크기
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+    
+	//S : DNA 길이, P : 부분문자열 길이(윈도우 사이즈)
+	int S, P;
 	cin >> S >> P;
-	int count = 0;		//가능한 비번 개수
-	string A;  //문자열 데이터
-	cin >> A;
+	
+	string DNA;
+	cin >> DNA;
 
+	//비교할 배열
+	int ACGT[4] = { 0, };
+	//체크할 문자 개수 A C G T 순서
+	int CheckChar[4] = { 0, };
 	for (int i = 0; i < 4; i++)
 	{
-		cin >> checkArr[i];
-		if (checkArr[i] == 0)
+		cin >> CheckChar[i];
+	}
+
+	int StartIndex = 0;
+	int EndIndex = P - 1;
+	int Count = 0;
+
+	for (int i = StartIndex; i <= EndIndex; i++)
+	{
+		if (DNA[i] == 'A')
+			ACGT[0]++;
+		else if (DNA[i] == 'C')
+			ACGT[1]++;
+		else if (DNA[i] == 'G')
+			ACGT[2]++;
+		else if (DNA[i] == 'T')
+			ACGT[3]++;
+	}
+
+	while (EndIndex < S)
+	{
+		
+		bool IsPossible = true;
+		//비밀번호 가능여부 체크
+		for (int i = 0; i < 4; i++)
 		{
-			checkSecret++;
+			if (ACGT[i] < CheckChar[i])
+			{
+				IsPossible = false;
+				break;
+			}
 		}
-	}
 
-	for (int i = 0; i < P; i++)  //초기 P부분 문자열 처리
-	{
-		Add(A[i]);
-	}
-	if (checkSecret == 4)
-	{
-		count++;
-	}
+		if (IsPossible) Count++;
 
-	for (int i = P; i < S; i++)
-	{
-		int j = i - P;
-		Add(A[i]);
-		Remove(A[j]);
+		char SubChar = DNA[StartIndex++];
+		char AddChar = DNA[++EndIndex];
 
-		if (checkSecret == 4)
-		{
-			count++;
-		}
+		if (SubChar == 'A')
+			ACGT[0]--;
+		else if (SubChar == 'C')
+			ACGT[1]--;
+		else if (SubChar == 'G')
+			ACGT[2]--;
+		else if (SubChar == 'T')
+			ACGT[3]--;
+
+		if (AddChar == 'A')
+			ACGT[0]++;
+		else if (AddChar == 'C')
+			ACGT[1]++;
+		else if (AddChar == 'G')
+			ACGT[2]++;
+		else if (AddChar == 'T')
+			ACGT[3]++;
 	}
-	cout << count++ << "\n";
+	cout << Count;
 
-}
-
-//새로 들어온 문자를 처리하는 함수
-void Add(char c)
-{
-	switch (c)
-	{
-	case 'A':
-		myArr[0]++;
-		if (myArr[0] == checkArr[0])
-			checkSecret++;
-		break;
-	case 'C':
-		myArr[1]++;
-		if (myArr[1] == checkArr[1])
-			checkSecret++;
-		break;
-	case 'G':
-		myArr[2]++;
-		if (myArr[2] == checkArr[2])
-			checkSecret++;
-		break;
-	case 'T':
-		myArr[3]++;
-		if (myArr[3] == checkArr[3])
-			checkSecret++;
-		break;
-	}
-}
-
-void Remove(char c)
-{
-	switch (c)
-	{
-	case 'A':
-		if (myArr[0] == checkArr[0])
-			checkSecret--;
-		myArr[0]--;
-		break;
-	case 'C':
-		if (myArr[1] == checkArr[1])
-			checkSecret--;
-		myArr[1]--;
-		break;
-	case 'G':
-		if (myArr[2] == checkArr[2])
-			checkSecret--;
-		myArr[2]--;
-		break;
-	case 'T':
-		if (myArr[3] == checkArr[3])
-			checkSecret--;
-		myArr[3]--;
-		break;
-	}
+	return 0;
 }
