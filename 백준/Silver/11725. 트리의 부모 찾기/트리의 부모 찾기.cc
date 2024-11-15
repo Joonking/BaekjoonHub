fd045fc91e7f1867e90b8b58c24369695cdef609
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2024-11-15 22:17:16 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 10932 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,46 +1,51 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-void DFS(vector<vector<int>> &tree, vector<bool> &visited, vector<int> &answer, int number)
+int N;
+
+vector<vector<int>> NodeList;
+vector<bool> Visited;
+vector<int> ParentNodeList;
+
+void DFS(int SelfNum, int ParentNum)
 {
-    visited[number] = true;
-	for(int i : tree[number])
+	if (Visited[SelfNum])
+		return;
+
+	Visited[SelfNum] = true;
+	ParentNodeList[SelfNum] = ParentNum;
+	for (int i : NodeList[SelfNum])
 	{
-		if(!visited[i])
-		{
-            answer[i] = number;
-            DFS(tree, visited, answer, i);
-		}
+		DFS(i, SelfNum);
 	}
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    int N;      //노드의 개수
-    cin >> N;
+	cin >> N;
+	NodeList.resize(N + 1);
+	ParentNodeList.resize(N + 1);
+	Visited.resize(N + 1, false);
 
-    vector<vector<int>> Tree(N+1);
-    vector<bool> Visited(N + 1, false);
-    vector<int> Answer(N + 1,0);
+	for (int i=0;i<N-1;i++)
+	{
+		int TempA, TempB;
+		cin >> TempA >> TempB;
+		NodeList[TempA].push_back(TempB);
+		NodeList[TempB].push_back(TempA);
+	}
 
-    for(int i=0;i<N-1;i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        Tree[a].push_back(b);
-        Tree[b].push_back(a);
-    }
 
-    DFS(Tree, Visited, Answer, 1);
+	DFS(1,0);
 
-    for(int i=2;i<N+1;i++)
-    {
-        cout << Answer[i] << "\n";
-    }
-   
+	for (int i = 2; i <= N; i++)
+		cout << ParentNodeList[i] << "\n";
+
+	return 0;
 }
