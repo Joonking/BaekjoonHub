c89f606c879737e6b189b161b16e5eab631fc4d2
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2025-03-10 23:06:33 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.69 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -4,26 +4,35 @@
 using namespace std;
 
 vector<int> solution(string s) {
-    int transform_count = 0; // 변환 횟수
-    int removed_zeros = 0;   // 제거한 0의 개수
-
-    while (s != "1") {
-        transform_count++;
-
-        // 0 제거 및 남은 길이 카운트
-        int ones_count = 0;
-        for (char c : s) {
-            if (c == '1') ones_count++;
-            else removed_zeros++;
+    
+    //이진 변환 횟수
+    int CaseCount = 0;
+    //제거된 모든 0의 개수
+    int ZeroCount = 0;
+    
+    while(s != "1")
+    {
+        CaseCount++;
+        int OneCount = 0;
+        for(char c : s)
+        {
+            if(c == '0')
+            {
+                ZeroCount++;
+            }
+            else if(c == '1')
+            {
+                OneCount++;
+            }
         }
-
-        // 남은 길이를 이진수로 변환
         s = "";
-        while (ones_count > 0) {
-            s = to_string(ones_count % 2) + s;
-            ones_count /= 2;
+        while(OneCount > 0)
+        {
+            s = to_string(OneCount%2) + s;
+            OneCount /= 2;
         }
+        
     }
-
-    return {transform_count, removed_zeros};
+    
+    return {CaseCount,ZeroCount};
 }
