338579f176f984003836fa068bb021a995e083c8
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2025-03-19 21:19:46 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,35 +1,37 @@
 #include <string>
 #include <vector>
+
 using namespace std;
 
 vector<bool> Visited;
 
-void DFS(vector<vector<int>> &list, int num)
+void DFS(const vector<vector<int>>& computers, int num)
 {
     if(Visited[num])
         return;
     Visited[num] = true;
     
-    for(int i=0; i<list.size();i++)
+    for(int i= 0; i<computers[num].size();i ++)
     {
-        if(list[num][i])
-            DFS(list, i);
+        if(computers[num][i] == 1 && i != num)
+            DFS(computers, i);
     }
-    
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
+    
     Visited = vector<bool>(n, false);
     
-    for(int i=0;i<n;i++)
+    for(int i=0;i<computers.size();i++)
     {
-        if(!Visited[i])
+        if(Visited[i] == false)
         {
             answer++;
             DFS(computers, i);
         }
     }
+    
     
     return answer;
 }
