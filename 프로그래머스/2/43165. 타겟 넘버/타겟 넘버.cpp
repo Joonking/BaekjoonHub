cfdffa000e139b33a22d57b031198f18bd52847c
Minjoon Kim <47269279+Joonking@users.noreply.github.com>
2025-03-19 21:01:22 +0900

    [level 2] Title: 타겟 넘버, Time: 3.88 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int answer = 0;
 
-void DFS(vector<int> numbers, int target, int sum, int index)
+void DFS(const vector<int>& numbers, int target, int sum, int index)
 {
     if(index == numbers.size())
     {
-        if(sum == target)
-            answer++;
+        if(target == sum) answer++;
         return;
     }
-    DFS(numbers, target, sum + numbers[index], index+1);
-    DFS(numbers, target, sum - numbers[index], index+1);
+    
+    DFS(numbers, target, sum+numbers[index], index+1);
+    DFS(numbers, target, sum-numbers[index], index+1);
 }
-
+    
 int solution(vector<int> numbers, int target) 
 {
     DFS(numbers, target, 0, 0);
